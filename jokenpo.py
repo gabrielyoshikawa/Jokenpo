a526f3921cc55594fc5da33bc791a2524975152c
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2022-06-07 21:05:20 -0300

    Finished Translation

--- jokenpo.py
+++ jokenpo.py
@@ -12,11 +12,11 @@ verification = True
 moves = ["Rock", "Paper", "Scissors"]
 
 # Menu
-gameMode = int(input("Choose a game mode: CPU X CPU(1), Player x Player(2), Player x CPU(3): "))
+game_mode = int(input("Choose a game mode: CPU X CPU(1), Player x Player(2), Player x CPU(3): "))
 while verification:
 
     # CPU vs CPU
-    if gameMode == 1:
+    if game_mode == 1:
         cpu1 = random.choice(moves)
         cpu2 = random.choice(moves)
         print("-----------------------------------")
@@ -36,132 +36,132 @@ while verification:
             print("CPU 1 Wins!")
             scpu2 += 1
 
-        # CPU 1 chosses paper
+        # CPU 1 chooses paper
     
         elif cpu1 == "Paper" and cpu2 == "Rock":
             print("CPU 1 Wins!")
             scpu1 += 1
         elif cpu1 == "Paper" and cpu2 == "Paper":
             print("Draw")
-        elif cpu2 == "Paper" and cpu2 == "Scissors":
+        elif cpu1 == "Paper" and cpu2 == "Scissors":
             print("CPU 2 Wins!")
             scpu2 += 1
 
-        # CPU 1 chosses scissors
+        # CPU 1 chooses scissors
 
         elif cpu1 == "Scissors" and cpu2 == "Rock":
             print("CPU 2 Wins!")
             scpu2 += 1
-        elif cpu2 == "Scissors" and cpu2 == "Paper":
+        elif cpu1 == "Scissors" and cpu2 == "Paper":
             print("CPU 1 Wins!")
             scpu1 += 1
-        else:
+        elif cpu1 == "Scissors" and cpu2 == "Scissors":
             print("Draw")
 
     # Player vs Player
 
-    elif gameMode == 2:
-        player1_action = input("Player 1 Turn \nDigite a sua jogada: Pedra, Papel ou Tesoura: ")
-        player2_action = input("Player 2 Turn \nDigite a sua jogada: Pedra, Papel ou Tesoura: ")
+    elif game_mode == 2:
+        player1_action = input("Player 1 Turn \nEnter a choice: Rock, Paper or Scissors: ")
+        player2_action = input("Player 2 Turn \nEnter a choice: Rock, Paper or Scissors: ")
         print("-----------------------------------")
         print("JO \nKEN \nPO")
         print("-----------------------------------")
-        print("O jogador 1 jogou: ", player1_action)
-        print("O jogador 2 jogou: ", player2_action)
+        print("Player 1 choose: ", player1_action)
+        print("Player 2 choose: ", player2_action)
 
-        # Jogador1 Pedra
+        # Player 2 chooses rock
 
-        if jogada1 == "Pedra" and jogada2 == "Pedra":
-            print("Empatou")
-        elif jogada1 == "Papel" and jogada2 == "Pedra":
-            print("Vitória do jogador 1!")
-            pl1 += 1
-        elif jogada1 == "Tesoura" and jogada2 == "Pedra":
-            print("Vitória do jogador 2!")
-            pl2 += 1
+        if player1_action == "Rock" and player2_action == "Rock":
+            print("Draw")
+        elif player1_action == "Paper" and player2_action == "Rock":
+            print("Player 1 Wins!")
+            spl1 += 1
+        elif player1_action == "Scissors" and player2_action == "Rock":
+            print("Player 2 Wins!")
+            spl2 += 1
 
-        # Jogador1 Papel
+        # Player 2 chooses paper
 
-        if jogada1 == "Pedra" and jogada2 == "Papel":
-            print("Vitória do jogador 2!")
-            pl2 += 1
-        elif jogada1 == "Papel" and jogada2 == "Papel":
-            print("Empatou")
-        elif jogada1 == "Tesoura" and jogada2 == "Papel":
-            print("Vitória do jogador 1")
-            pl1 += 1
+        if player1_action == "Rock" and player2_action == "Paper":
+            print("Player 2 Wins!")
+            spl2 += 1
+        elif player1_action == "Paper" and player2_action == "Paper":
+            print("Draw")
+        elif player1_action == "Scissors" and player2_action == "Paper":
+            print("Player 1 Wins!")
+            spl1 += 1
 
-        # Jogador1 Tesoura
+        # Player 2 chooses scissors
 
-        if jogada1 == "Pedra" and jogada2 == "Tesoura":
-            print("Vitória do jogador 1!")
-            pl1 += 1
-        elif jogada1 == "Papel" and jogada2 == "Tesoura":
-            print("Vitória do jogador 2!")
-            pl2 += 1
-        elif jogada1 == "Tesoura" and jogada2 == "Tesoura":
-            print("Empatou")
+        if player1_action == "Rock" and player2_action == "Scissors":
+            print("Player 1 Wins!")
+            spl1 += 1
+        elif player1_action == "Paper" and player2_action == "Scissors":
+            print("Player 2 Wins!")
+            spl2 += 1
+        elif player1_action == "Scissors" and player2_action == "Scissors":
+            print("Draw")
 
-    # Player vs PC
+    # Player vs CPU
 
     else:
-        computador = random.choice(lista1)
-        jogada = input("Digite a sua jogada: Pedra, Papel e Tesoura: ")
+        cpu3 = random.choice(moves)
+        player3_action = input("Enter a choice: Rock, Paper or Scissors: ")
         print("-----------------------------------")
         print("JO \nKEN \nPO")
         print("-----------------------------------")
-        print("O computador jogou:", computador)
-        print("Você jogou:", jogada)
+        print("CPU chose:", cpu3)
+        print("You chose:", player3_action)
 
-        # Jogador Pedra
+        # Player chooses rock
 
-        if computador == "Pedra" and jogada == "Pedra":
-            print("Empatou")
-        elif computador == "Papel" and jogada == "Pedra":
-            print("Você perdeu")
-            pc3 += 1
-        elif computador == "Tesoura" and jogada == "Pedra":
-            print("Você ganhou!")
-            pl3 += 1
+        if cpu3 == "Rock" and player3_action == "Rock":
+            print("Draw")
+        elif cpu3 == "Paper" and player3_action == "Rock":
+            print("You lose")
+            scpu3 += 1
+        elif cpu3 == "Scissors" and player3_action == "Rock":
+            print("You win!")
+            spl3 += 1
 
-        # Jogador Papel
+        # Player chooses paper
 
-        if computador == "Pedra" and jogada == "Papel":
-            print("Você ganhou!")
-            pl3 += 1
-        elif computador == "Papel" and jogada == "Papel":
-            print("Empatou")
-        elif computador == "Tesoura" and jogada == "Papel":
-            print("Você perdeu")
-            pc3 += 1
+        if cpu3 == "Rock" and player3_action == "Paper":
+            print("You win!")
+            spl3 += 1
+        elif cpu3 == "Paper" and player3_action == "Paper":
+            print("Draw")
+        elif cpu3 == "Scissors" and player3_action == "Paper":
+            print("You lose")
+            scpu3 += 1
 
-        # Jogador Tesoura
+        # Player chooses scissors
 
-        if computador == "Pedra" and jogada == "Tesoura":
-            print("Você perdeu")
-            pc3 += 1
-        elif computador == "Papel" and jogada == "Tesoura":
-            print("Você ganhou!")
-            pl3 += 1
-        elif computador == "Tesoura" and jogada == "Tesoura":
-            print("Empatou")
+        if cpu3 == "Rock" and player3_action == "Scissors":
+            print("You lose")
+            scpu3 += 1
+        elif cpu3 == "Paper" and player3_action == "Scissors":
+            print("You win!")
+            spl3 += 1
+        elif cpu3 == "Scissors" and player3_action == "Scissors":
+            print("Draw")
 
-    # Encerramento
+    # Play again
 
-    continuar = int(input("Deseja jogar novamente? Sim(1) Não(2): "))
+    play_again = int(input("Play again? Yes(1) No(2): "))
     print1 = 0
     print2 = 0
-    if continuar == 1:
-        verificacao = True
+    if play_again == 1:
+        verification = True
     else:
-        if modo == 1:
-            print1 = pc1
-            print2 = pc2
-        elif modo == 2:
-            print1 = pl1
-            print2 = pl2
+        if game_mode == 1:
+            print1 = scpu1
+            print2 = scpu2
+        elif game_mode == 2:
+            print1 = spl1
+            print2 = spl2
         else:
-            print1 = pl3
-            print2 = pc3
+            print1 = spl3
+            print2 = scpu3
         print("-----------------------------------\nThanks for playing \nScoreboard: User 1: {0} / User 2: {1} ".format(print1, print2))
-        verificacao = False
+        verification = False
